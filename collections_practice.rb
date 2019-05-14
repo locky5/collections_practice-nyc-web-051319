dd68d0d98259fb283b874de319b9facf767f0b9a
Kevin Dai <daikevin19@yahoo.com>
2019-05-14 15:52:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by { |word| word.length}
+end
+
+def swap_elements(array)
+  temp_element = array[1]
+  array[1] = array[2]
+  array[2] = temp_element
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |string|
+    string[2] = "$"
+  end
+  return array
+end
+
+def find_a(array)
+  array.select { |string| string[0] == "a" }
+end
+
+def sum_array(array)
+  sum = 0 
+  array.map do |integer|
+    sum = sum + integer
+  end
+  sum
+end
+
+def add_s(array)
+  new_array = array.select.with_index { |word,index| word if index != 1 }
+  final_array = new_array.map do |element|
+    element + "s"
+  end
+  final_array.insert(1, array[1])
+end
